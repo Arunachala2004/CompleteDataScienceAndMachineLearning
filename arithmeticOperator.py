540f77045b30499d6efaac6ee0db61cf85c766c2
Arunachala <93348029+Arunachala2004@users.noreply.github.com>
2024-05-25 23:46:39 +0530

    Create arithmeticOperator.py

---
+++ arithmeticOperator.py
@@ -0,0 +1,19 @@
+def utility():
+    x=int(input())
+    y=int(input())
+    
+    # Perform addition x+y below
+    p = x + y
+    # Perform subtraction x-y below
+    q = x - y
+    # Perform multiplication x*y below
+    r = x * y
+    # Perform float division x/y below
+    s = x / y
+    # Perform integer divison x//y below
+    t = x // y
+    # Perform modulo operation x%y below
+    u = x % y
+    # Perform power(x,y) x**y below
+    v = x ** y
+    print(p,q,r,"{:.5f}".format(s),t,u,v)
